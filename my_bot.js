bd8e7d68bcdf7c6ba79b983ea0e4a0fd4a66be1f
Tariq Aziz <46774473+tariqaziz123@users.noreply.github.com>
2020-10-01 17:43:48 +0530

    Add files via upload

---
+++ my_bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix='-';
+client.once('ready', () =>{
+    console.log("Connected as " + client.user.tag)
+    client.user.setActivity("Youtube", {type:"WATCHING"})
+   /* client.guilds.cache.forEach((guild) => {
+        console.log(guild.name)
+        guild.channels.cache.forEach((channel) => {
+            console.log(`- ${channel.name} ${channel.type} ${channel.id}`)
+        });
+    });
+    var generalChannel = client.channels.cache.get("761104476757033011")
+    generalChannel.send('Hello, world!!')*/
+});
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!')
+    }
+    else if(command === 'hi'){
+        message.channel.send('Welcome, How can I help you??')
+    }
+    else if(command === 'gyan'){
+        message.channel.send('He is strong')
+    }
+    else if(command === 'shizuka'){
+        message.channel.send('She is  cute')
+    }
+    else if(command === 'neymar'){
+        message.channel.send('Don`t take my name...I will kick')
+    }
+}) 
+
+client.login('NzYxMTA1MzM5MzA2OTk5ODE4.X3VwlQ.uZIq6KUuCMJym529NriqSPUKeb0')
